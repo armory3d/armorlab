03464864bf85c2a93bf3803e09fd5c3581d402f0
Lubos Lenco <lubos.lenco@gmail.com>
2021-08-09 19:17:24 +0200

    Update flags

--- kincflags.js
+++ kincflags.js
@@ -2,6 +2,13 @@
 // Imported by armorcore/kincfile.js
 module.exports = {
 	set_flags: function (flags) {
+		flags.name = 'ArmorLab';
+		flags.package = 'org.armorlab';
+		flags.with_d3dcompiler = true;
+		flags.with_nfd = true;
+		flags.with_tinydir = true;
+		flags.with_zlib = true;
+		flags.with_stb_image_write = true;
 		flags.with_texsynth = true;
 		flags.with_onnx = true;
 	}
