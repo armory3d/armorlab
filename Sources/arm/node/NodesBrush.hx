351ead9d68aedf5de9cb9fd7293de4c979b88410
Lubos Lenco <lubos.lenco@gmail.com>
2021-08-13 13:06:13 +0200

    Init

--- Sources/arm/node/NodesBrush.hx
+++ Sources/arm/node/NodesBrush.hx
@@ -65,6 +65,35 @@ class NodesBrush {
 			},
 			{
 				id: 0,
+				name: _tr("Inpaint"),
+				type: "InpaintNode",
+				x: 0,
+				y: 0,
+				color: 0xff4982a0,
+				inputs: [
+					{
+						id: 0,
+						node_id: 0,
+						name: _tr("Color"),
+						type: "VECTOR",
+						color: 0xffc7c729,
+						default_value: f32([0.0, 0.0, 0.0])
+					}
+				],
+				outputs: [
+					{
+						id: 0,
+						node_id: 0,
+						name: _tr("Color"),
+						type: "VECTOR",
+						color: 0xffc7c729,
+						default_value: f32([0.0, 0.0, 0.0, 1.0])
+					}
+				],
+				buttons: []
+			},
+			{
+				id: 0,
 				name: _tr("Photo to PBR"),
 				type: "PhotoToPBRNode",
 				x: 0,
@@ -132,35 +161,7 @@ class NodesBrush {
 				],
 				buttons: []
 			},
-			{
-				id: 0,
-				name: _tr("Inpaint"),
-				type: "InpaintNode",
-				x: 0,
-				y: 0,
-				color: 0xff4982a0,
-				inputs: [
-					{
-						id: 0,
-						node_id: 0,
-						name: _tr("Color"),
-						type: "VECTOR",
-						color: 0xffc7c729,
-						default_value: f32([0.0, 0.0, 0.0])
-					}
-				],
-				outputs: [
-					{
-						id: 0,
-						node_id: 0,
-						name: _tr("Color"),
-						type: "VECTOR",
-						color: 0xffc7c729,
-						default_value: f32([0.0, 0.0, 0.0, 1.0])
-					}
-				],
-				buttons: []
-			},
+
 			{
 				id: 0,
 				name: _tr("Tiling"),
@@ -189,269 +190,269 @@ class NodesBrush {
 					}
 				],
 				buttons: []
-			},
-			{
-				id: 0,
-				name: _tr("Math"),
-				type: "MathNode",
-				x: 0,
-				y: 0,
-				color: 0xff4982a0,
-				inputs: [
-					{
-						id: 0,
-						node_id: 0,
-						name: _tr("Value"),
-						type: "VALUE",
-						color: 0xffa1a1a1,
-						default_value: 0.5
-					},
-					{
-						id: 0,
-						node_id: 0,
-						name: _tr("Value"),
-						type: "VALUE",
-						color: 0xffa1a1a1,
-						default_value: 0.5
-					}
-				],
-				outputs: [
-					{
-						id: 0,
-						node_id: 0,
-						name: _tr("Value"),
-						type: "VALUE",
-						color: 0xffa1a1a1,
-						default_value: 0.0
-					}
-				],
-				buttons: [
-					{
-						name: _tr("operation"),
-						type: "ENUM",
-						data: ["Add", "Subtract", "Multiply", "Divide", "Power", "Logarithm", "Square Root", "Inverse Square Root", "Absolute", "Exponent", "Minimum", "Maximum", "Less Than", "Greater Than", "Sign", "Round", "Floor", "Ceil", "Truncate", "Fraction", "Modulo", "Snap", "Ping-Pong", "Sine", "Cosine", "Tangent", "Arcsine", "Arccosine", "Arctangent", "Arctan2", "Hyperbolic Sine", "Hyperbolic Cosine", "Hyperbolic Tangent", "To Radians", "To Degrees"],
-						default_value: 0,
-						output: 0
-					},
-					{
-						name: _tr("use_clamp"),
-						type: "BOOL",
-						default_value: false,
-						output: 0
-					}
-				]
-			},
-			{
-				id: 0,
-				name: _tr("Random"),
-				type: "RandomNode",
-				x: 0,
-				y: 0,
-				color: 0xffb34f5a,
-				inputs: [
-					{
-						id: 0,
-						node_id: 0,
-						name: _tr("Min"),
-						type: "VALUE",
-						color: 0xffa1a1a1,
-						default_value: 0.0
-					},
-					{
-						id: 0,
-						node_id: 0,
-						name: _tr("Max"),
-						type: "VALUE",
-						color: 0xffa1a1a1,
-						default_value: 1.0
-					}
-				],
-				outputs: [
-					{
-						id: 0,
-						node_id: 0,
-						name: _tr("Value"),
-						type: "VALUE",
-						color: 0xffa1a1a1,
-						default_value: 0.5
-					}
-				],
-				buttons: []
-			},
-			{
-				id: 0,
-				name: _tr("Separate Vector"),
-				type: "SeparateVectorNode",
-				x: 0,
-				y: 0,
-				color: 0xff4982a0,
-				inputs: [
-					{
-						id: 0,
-						node_id: 0,
-						name: _tr("Vector"),
-						type: "VECTOR",
-						color: 0xff6363c7,
-						default_value: f32([0.0, 0.0, 0.0])
-					}
-				],
-				outputs: [
-					{
-						id: 0,
-						node_id: 0,
-						name: _tr("X"),
-						type: "VALUE",
-						color: 0xffa1a1a1,
-						default_value: 0.0
-					},
-					{
-						id: 0,
-						node_id: 0,
-						name: _tr("Y"),
-						type: "VALUE",
-						color: 0xffa1a1a1,
-						default_value: 0.0
-					},
-					{
-						id: 0,
-						node_id: 0,
-						name: _tr("Z"),
-						type: "VALUE",
-						color: 0xffa1a1a1,
-						default_value: 0.0
-					}
-				],
-				buttons: []
-			},
-			{
-				id: 0,
-				name: _tr("Value"),
-				type: "FloatNode",
-				x: 0,
-				y: 0,
-				color: 0xffb34f5a,
-				inputs: [
-					{
-						id: 0,
-						node_id: 0,
-						name: _tr("Value"),
-						type: "VALUE",
-						color: 0xffa1a1a1,
-						default_value: 0.5,
-						min: 0.0,
-						max: 10.0
-					}
-				],
-				outputs: [
-					{
-						id: 0,
-						node_id: 0,
-						name: _tr("Value"),
-						type: "VALUE",
-						color: 0xffa1a1a1,
-						default_value: 0.5
-					}
-				],
-				buttons: []
-			},
-			{
-				id: 0,
-				name: _tr("Vector"),
-				type: "VectorNode",
-				x: 0,
-				y: 0,
-				color: 0xff4982a0,
-				inputs: [
-					{
-						id: 0,
-						node_id: 0,
-						name: _tr("X"),
-						type: "VALUE",
-						color: 0xffa1a1a1,
-						default_value: 0.0
-					},
-					{
-						id: 0,
-						node_id: 0,
-						name: _tr("Y"),
-						type: "VALUE",
-						color: 0xffa1a1a1,
-						default_value: 0.0
-					},
-					{
-						id: 0,
-						node_id: 0,
-						name: _tr("Z"),
-						type: "VALUE",
-						color: 0xffa1a1a1,
-						default_value: 0.0
-					}
-				],
-				outputs: [
-					{
-						id: 0,
-						node_id: 0,
-						name: _tr("Vector"),
-						type: "VECTOR",
-						color: 0xff6363c7,
-						default_value: f32([0.0, 0.0, 0.0])
-					}
-				],
-				buttons: []
-			},
-			{
-				id: 0,
-				name: _tr("Vector Math"),
-				type: "VectorMathNode",
-				x: 0,
-				y: 0,
-				color: 0xff4982a0,
-				inputs: [
-					{
-						id: 0,
-						node_id: 0,
-						name: _tr("Vector"),
-						type: "VECTOR",
-						color: 0xff6363c7,
-						default_value: f32([0.0, 0.0, 0.0])
-					},
-					{
-						id: 0,
-						node_id: 0,
-						name: _tr("Vector"),
-						type: "VECTOR",
-						color: 0xff6363c7,
-						default_value: f32([0.0, 0.0, 0.0])
-					}
-				],
-				outputs: [
-					{
-						id: 0,
-						node_id: 0,
-						name: _tr("Vector"),
-						type: "VECTOR",
-						color: 0xff6363c7,
-						default_value: f32([0.0, 0.0, 0.0])
-					},
-					{
-						id: 0,
-						node_id: 0,
-						name: _tr("Value"),
-						type: "VALUE",
-						color: 0xffa1a1a1,
-						default_value: 0.0
-					}
-				],
-				buttons: [
-					{
-						name: _tr("operation"),
-						type: "ENUM",
-						data: ["Add", "Subtract", "Multiply", "Divide", "Average", "Cross Product", "Project", "Reflect", "Dot Product", "Distance", "Length", "Scale", "Normalize", "Absolute", "Minimum", "Maximum", "Floor", "Ceil", "Fraction", "Modulo", "Snap", "Sine", "Cosine", "Tangent"],
-						default_value: 0,
-						output: 0
-					}
-				]
-			}
+			}//,
+			// {
+			// 	id: 0,
+			// 	name: _tr("Math"),
+			// 	type: "MathNode",
+			// 	x: 0,
+			// 	y: 0,
+			// 	color: 0xff4982a0,
+			// 	inputs: [
+			// 		{
+			// 			id: 0,
+			// 			node_id: 0,
+			// 			name: _tr("Value"),
+			// 			type: "VALUE",
+			// 			color: 0xffa1a1a1,
+			// 			default_value: 0.5
+			// 		},
+			// 		{
+			// 			id: 0,
+			// 			node_id: 0,
+			// 			name: _tr("Value"),
+			// 			type: "VALUE",
+			// 			color: 0xffa1a1a1,
+			// 			default_value: 0.5
+			// 		}
+			// 	],
+			// 	outputs: [
+			// 		{
+			// 			id: 0,
+			// 			node_id: 0,
+			// 			name: _tr("Value"),
+			// 			type: "VALUE",
+			// 			color: 0xffa1a1a1,
+			// 			default_value: 0.0
+			// 		}
+			// 	],
+			// 	buttons: [
+			// 		{
+			// 			name: _tr("operation"),
+			// 			type: "ENUM",
+			// 			data: ["Add", "Subtract", "Multiply", "Divide", "Power", "Logarithm", "Square Root", "Inverse Square Root", "Absolute", "Exponent", "Minimum", "Maximum", "Less Than", "Greater Than", "Sign", "Round", "Floor", "Ceil", "Truncate", "Fraction", "Modulo", "Snap", "Ping-Pong", "Sine", "Cosine", "Tangent", "Arcsine", "Arccosine", "Arctangent", "Arctan2", "Hyperbolic Sine", "Hyperbolic Cosine", "Hyperbolic Tangent", "To Radians", "To Degrees"],
+			// 			default_value: 0,
+			// 			output: 0
+			// 		},
+			// 		{
+			// 			name: _tr("use_clamp"),
+			// 			type: "BOOL",
+			// 			default_value: false,
+			// 			output: 0
+			// 		}
+			// 	]
+			// },
+			// {
+			// 	id: 0,
+			// 	name: _tr("Random"),
+			// 	type: "RandomNode",
+			// 	x: 0,
+			// 	y: 0,
+			// 	color: 0xffb34f5a,
+			// 	inputs: [
+			// 		{
+			// 			id: 0,
+			// 			node_id: 0,
+			// 			name: _tr("Min"),
+			// 			type: "VALUE",
+			// 			color: 0xffa1a1a1,
+			// 			default_value: 0.0
+			// 		},
+			// 		{
+			// 			id: 0,
+			// 			node_id: 0,
+			// 			name: _tr("Max"),
+			// 			type: "VALUE",
+			// 			color: 0xffa1a1a1,
+			// 			default_value: 1.0
+			// 		}
+			// 	],
+			// 	outputs: [
+			// 		{
+			// 			id: 0,
+			// 			node_id: 0,
+			// 			name: _tr("Value"),
+			// 			type: "VALUE",
+			// 			color: 0xffa1a1a1,
+			// 			default_value: 0.5
+			// 		}
+			// 	],
+			// 	buttons: []
+			// },
+			// {
+			// 	id: 0,
+			// 	name: _tr("Separate Vector"),
+			// 	type: "SeparateVectorNode",
+			// 	x: 0,
+			// 	y: 0,
+			// 	color: 0xff4982a0,
+			// 	inputs: [
+			// 		{
+			// 			id: 0,
+			// 			node_id: 0,
+			// 			name: _tr("Vector"),
+			// 			type: "VECTOR",
+			// 			color: 0xff6363c7,
+			// 			default_value: f32([0.0, 0.0, 0.0])
+			// 		}
+			// 	],
+			// 	outputs: [
+			// 		{
+			// 			id: 0,
+			// 			node_id: 0,
+			// 			name: _tr("X"),
+			// 			type: "VALUE",
+			// 			color: 0xffa1a1a1,
+			// 			default_value: 0.0
+			// 		},
+			// 		{
+			// 			id: 0,
+			// 			node_id: 0,
+			// 			name: _tr("Y"),
+			// 			type: "VALUE",
+			// 			color: 0xffa1a1a1,
+			// 			default_value: 0.0
+			// 		},
+			// 		{
+			// 			id: 0,
+			// 			node_id: 0,
+			// 			name: _tr("Z"),
+			// 			type: "VALUE",
+			// 			color: 0xffa1a1a1,
+			// 			default_value: 0.0
+			// 		}
+			// 	],
+			// 	buttons: []
+			// },
+			// {
+			// 	id: 0,
+			// 	name: _tr("Value"),
+			// 	type: "FloatNode",
+			// 	x: 0,
+			// 	y: 0,
+			// 	color: 0xffb34f5a,
+			// 	inputs: [
+			// 		{
+			// 			id: 0,
+			// 			node_id: 0,
+			// 			name: _tr("Value"),
+			// 			type: "VALUE",
+			// 			color: 0xffa1a1a1,
+			// 			default_value: 0.5,
+			// 			min: 0.0,
+			// 			max: 10.0
+			// 		}
+			// 	],
+			// 	outputs: [
+			// 		{
+			// 			id: 0,
+			// 			node_id: 0,
+			// 			name: _tr("Value"),
+			// 			type: "VALUE",
+			// 			color: 0xffa1a1a1,
+			// 			default_value: 0.5
+			// 		}
+			// 	],
+			// 	buttons: []
+			// },
+			// {
+			// 	id: 0,
+			// 	name: _tr("Vector"),
+			// 	type: "VectorNode",
+			// 	x: 0,
+			// 	y: 0,
+			// 	color: 0xff4982a0,
+			// 	inputs: [
+			// 		{
+			// 			id: 0,
+			// 			node_id: 0,
+			// 			name: _tr("X"),
+			// 			type: "VALUE",
+			// 			color: 0xffa1a1a1,
+			// 			default_value: 0.0
+			// 		},
+			// 		{
+			// 			id: 0,
+			// 			node_id: 0,
+			// 			name: _tr("Y"),
+			// 			type: "VALUE",
+			// 			color: 0xffa1a1a1,
+			// 			default_value: 0.0
+			// 		},
+			// 		{
+			// 			id: 0,
+			// 			node_id: 0,
+			// 			name: _tr("Z"),
+			// 			type: "VALUE",
+			// 			color: 0xffa1a1a1,
+			// 			default_value: 0.0
+			// 		}
+			// 	],
+			// 	outputs: [
+			// 		{
+			// 			id: 0,
+			// 			node_id: 0,
+			// 			name: _tr("Vector"),
+			// 			type: "VECTOR",
+			// 			color: 0xff6363c7,
+			// 			default_value: f32([0.0, 0.0, 0.0])
+			// 		}
+			// 	],
+			// 	buttons: []
+			// },
+			// {
+			// 	id: 0,
+			// 	name: _tr("Vector Math"),
+			// 	type: "VectorMathNode",
+			// 	x: 0,
+			// 	y: 0,
+			// 	color: 0xff4982a0,
+			// 	inputs: [
+			// 		{
+			// 			id: 0,
+			// 			node_id: 0,
+			// 			name: _tr("Vector"),
+			// 			type: "VECTOR",
+			// 			color: 0xff6363c7,
+			// 			default_value: f32([0.0, 0.0, 0.0])
+			// 		},
+			// 		{
+			// 			id: 0,
+			// 			node_id: 0,
+			// 			name: _tr("Vector"),
+			// 			type: "VECTOR",
+			// 			color: 0xff6363c7,
+			// 			default_value: f32([0.0, 0.0, 0.0])
+			// 		}
+			// 	],
+			// 	outputs: [
+			// 		{
+			// 			id: 0,
+			// 			node_id: 0,
+			// 			name: _tr("Vector"),
+			// 			type: "VECTOR",
+			// 			color: 0xff6363c7,
+			// 			default_value: f32([0.0, 0.0, 0.0])
+			// 		},
+			// 		{
+			// 			id: 0,
+			// 			node_id: 0,
+			// 			name: _tr("Value"),
+			// 			type: "VALUE",
+			// 			color: 0xffa1a1a1,
+			// 			default_value: 0.0
+			// 		}
+			// 	],
+			// 	buttons: [
+			// 		{
+			// 			name: _tr("operation"),
+			// 			type: "ENUM",
+			// 			data: ["Add", "Subtract", "Multiply", "Divide", "Average", "Cross Product", "Project", "Reflect", "Dot Product", "Distance", "Length", "Scale", "Normalize", "Absolute", "Minimum", "Maximum", "Floor", "Ceil", "Fraction", "Modulo", "Snap", "Sine", "Cosine", "Tangent"],
+			// 			default_value: 0,
+			// 			output: 0
+			// 		}
+			// 	]
+			// }
 		]
 	];
 
